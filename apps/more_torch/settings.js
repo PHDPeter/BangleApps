9e62e5c51e43fa0c5e69ada834f68095fd17f7b2
Peter Overbury <peteroverbury@gmail.com>
2021-01-07 21:41:25 +0000

    Update settings.js

--- apps/more_torch/settings.js
+++ apps/more_torch/settings.js
@@ -1,17 +1,47 @@
 // make sure to enclose the function in parentheses
 (function(back) {
-  let settings = require('Storage').readJSON('app.json',1)||{};
-  function save(key, value) {
-    settings[key] = value;
-    require('Storage').write('app.json',settings);
+  const Storage = require("Storage");
+  const filename = 'more_torch.json';
+  let settings = Storage.readJSON(filename,1)|| null;
+  
+  //setting the defults
+  function getSettings(){
+    return {
+      colour: 'red'
+    };
   }
+  
+  function updateSettings() {
+    require("Storage").writeJSON(filename, settings);
+    Bangle.buzz();
+  }
+   //if no setting add the defult
+  if(!settings){
+    settings = getSettings();
+    updateSettings();
+  }
+  //need for saving the setting when done
+  function saveChange(name){
+    return function(v){
+      settings[name] = v;
+      updateSettings();
+    }
+  }
+  //this is the items in the menu.
+  var colour_l = ['red', "blue"];
   const appMenu = {
-    '': {'title': 'App Settings'},
-    '< Back': back,
-    'Monkeys': {
-      value: settings.monkeys||12,
-      onchange: (m) => {save('monkeys', m)}
-    }   
+    '': {'title': 'More Torch Settings'},
+    "Colour " : {
+      value : 0 | colour_l.indexOf(settings.colour),
+      format: v => colour_l[v],
+      onchange: v => {
+        settings.HID = colour_l[v];
+        saveChange('colour');
+      }
+      //min: 1, max: 10, step: 1,
+      //onchange : saveChange('colour')
+    },
+    '< Back': back 
   };
   E.showMenu(appMenu)
 })
